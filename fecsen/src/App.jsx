9f8270d771b03af680560202578aa71624e2ea55
gyeone <voov000@naver.com>
2025-10-18 22:02:37 +0900

    feat/App.jsx 라우팅 설정

--- fecsen/src/App.jsx
+++ fecsen/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Main from "./pages/Main.jsx";
+import Footer from "./components/Footer.jsx";
+
 function App() {
-    return <div>app</div>;
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Main />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
