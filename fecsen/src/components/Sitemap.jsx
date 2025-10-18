10c222275a7c8c5d8982967724efe0d72dd2ca1e
gyeone <voov000@naver.com>
2025-10-18 23:08:56 +0900

    feat/Sitemap.jsx 시맨틱 태그와 클래스 네임 설정

---
+++ fecsen/src/components/Sitemap.jsx
@@ -0,0 +1,10 @@
+function Sitemap() {
+    return (
+        <section className="sitemap">
+            <h3 className="sitemap__menu"></h3>
+            <ul id="lnb"></ul>
+        </section>
+    );
+}
+
+export default Sitemap;
