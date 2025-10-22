152ad0c5fbb1aec9a25b11dd30b8ae9d1cc2fd8d
gyeone <voov000@naver.com>
2025-10-22 22:44:56 +0900

    feat/Footer.jsx의 언어 변경 기능 설정

--- fecsen/src/components/Footer.jsx
+++ fecsen/src/components/Footer.jsx
@@ -1,24 +1,27 @@
 import blackLogo from "../assets/icons/blackLogo.png";
 import { MdEmail } from "react-icons/md";
 import { IoCall } from "react-icons/io5";
+import { useTranslation } from "react-i18next";
 
 function Footer() {
+    const { t } = useTranslation();
+
     return (
         <footer className="footer">
             <img className="footer__logo" src={blackLogo} alt="펙센" />
             <div>
                 <ul id="fnb">
                     <li>
-                        <a href=""></a>
+                        <a href="">{t(`gnb.company-introduction`)}</a>
                     </li>
                     <li>
-                        <a href=""></a>
+                        <a href="">{t(`gnb.network-security`)}</a>
                     </li>
                     <li>
-                        <a href=""></a>
+                        <a href="">{t(`gnb.cloud`)}</a>
                     </li>
                     <li>
-                        <a href=""></a>
+                        <a href="">{t(`gnb.security-solutions`)}</a>
                     </li>
                 </ul>
                 <ul className="footer__contact">
