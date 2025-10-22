ad354949a1eac8e4472e024539ead61a736d18b0
gyeone <voov000@naver.com>
2025-10-22 22:34:50 +0900

    feat/Header.jsx의 언어 변경 기능 설정

--- fecsen/src/components/Header.jsx
+++ fecsen/src/components/Header.jsx
@@ -1,8 +1,20 @@
 import blackLogo from "../assets/icons/blackLogo.png";
 import whiteLogo from "../assets/icons/whiteLogo.png";
 import { MdLanguage } from "react-icons/md";
+import { useTranslation } from "react-i18next";
+import i18n from "../locales/i18n";
+import { useState } from "react";
 
 function Header() {
+    const { t } = useTranslation();
+    const [currentLanguage, setCurrentLanguage] = useState("KO");
+
+    const changeLanguage = () => {
+        const newLang = currentLanguage === "KO" ? "en" : "ko";
+        setCurrentLanguage(newLang.toUpperCase());
+        i18n.changeLanguage(newLang);
+    };
+
     return (
         <header className="header">
             <h1>
@@ -14,23 +26,28 @@ function Header() {
                 <h2 className="sr-only">메인 메뉴</h2>
                 <ul id="gnb">
                     <li>
-                        <a href=""></a>
+                        <a href="">{t(`header.company-introduction`)}</a>
                     </li>
                     <li>
-                        <a href=""></a>
+                        <a href="">{t(`header.network-security`)}</a>
                     </li>
                     <li>
-                        <a href=""></a>
+                        <a href="">{t(`header.cloud`)}</a>
                     </li>
                     <li>
-                        <a href=""></a>
+                        <a href="">{t(`header.security-solutions`)}</a>
                     </li>
                 </ul>
             </nav>
-            <button id="language-btn">
+            <label htmlFor="language-btn">
                 <MdLanguage />
-                KO
-            </button>
+                <input
+                    type="button"
+                    id="language-btn"
+                    value={currentLanguage}
+                    onClick={changeLanguage}
+                />
+            </label>
         </header>
     );
 }
