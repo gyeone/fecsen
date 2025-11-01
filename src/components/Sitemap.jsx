546eec872e39e077b0b499e1797ec2ca89509aaa
gyeone <voov000@naver.com>
2025-11-01 22:40:20 +0900

    feat/사이트맵의 마우스 호버와 리브했을 때의 기능과 메인 메뉴에 따른 상세 메뉴 보여주는 기능 구현

--- src/components/Sitemap.jsx
+++ src/components/Sitemap.jsx
@@ -1,8 +1,27 @@
-function Sitemap() {
+import { useTranslation } from "react-i18next";
+
+function Sitemap({ activeMenu, activeSitemap, setActiveSitemap }) {
+    const { t } = useTranslation();
+
+    if (!activeSitemap) return null;
+
+    const lnbItems = t(`lnb.${activeMenu}`, { returnObjects: true });
+
     return (
-        <section className="sitemap">
-            <h3 className="sitemap__menu"></h3>
-            <ul id="lnb"></ul>
+        <section
+            className={`sitemap ${activeMenu ? "sitemap--visible" : ""}`}
+            onMouseEnter={() => setActiveSitemap(true)}
+            onMouseLeave={() => setActiveSitemap(false)}
+        >
+            <h3 id="sitemap__menu">{t(`gnb.${activeMenu}`)}</h3>
+            <ul id="lnb">
+                {lnbItems &&
+                    Object.entries(lnbItems).map(([k, v]) => (
+                        <li key={k}>
+                            <a href="">{v}</a>
+                        </li>
+                    ))}
+            </ul>
         </section>
     );
 }
