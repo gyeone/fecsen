2f53988ab40723d534363d90f7c868191e6de36b
gyeone <voov000@naver.com>
2025-11-01 22:36:54 +0900

    feat/헤더에서 마우스 떠나면 사이트맵 안 보이는 기능 구현, 설명 주석 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,19 @@ import whiteLogo from "../assets/icons/whiteLogo.png";
 import { MdLanguage } from "react-icons/md";
 import { useTranslation } from "react-i18next";
 import i18n from "../locales/i18n";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Sitemap from "./Sitemap";
 
 function Header() {
     const { t } = useTranslation();
+    // 현재 언어
     const [currentLanguage, setCurrentLanguage] = useState("KO");
+    // 메인 메뉴 이름
+    const [activeMenu, setActiveMenu] = useState(null);
+    // 사이트맵 여부
+    const [activeSitemap, setActiveSitemap] = useState(false);
 
+    // 언어 변경 함수
     const changeLanguage = () => {
         const newLang = currentLanguage === "KO" ? "en" : "ko";
         setCurrentLanguage(newLang.toUpperCase());
@@ -16,47 +23,81 @@ function Header() {
     };
 
     return (
-        <header className="header">
-            <h1>
-                <a href="/">
-                    <img id="header__logo-img" src={blackLogo} alt="펙센" />
-                </a>
-            </h1>
-            <nav>
-                <h2 className="sr-only">메인 메뉴</h2>
-                <ul id="gnb">
-                    <li className="gnb__menu">
-                        <a className="gnb__link" href="">
-                            {t(`gnb.company-introduction`)}
-                        </a>
-                    </li>
-                    <li className="gnb__menu">
-                        <a className="gnb__link" href="">
-                            {t(`gnb.network-security`)}
-                        </a>
-                    </li>
-                    <li className="gnb__menu">
-                        <a className="gnb__link" href="">
-                            {t(`gnb.cloud`)}
-                        </a>
-                    </li>
-                    <li className="gnb__menu">
-                        <a className="gnb__link" href="">
-                            {t(`gnb.security-solutions`)}
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-            <label id="language-change" htmlFor="language-btn">
-                <MdLanguage id="language-img" />
-                <input
-                    type="button"
-                    id="language-btn"
-                    value={currentLanguage}
-                    onClick={changeLanguage}
-                />
-            </label>
-        </header>
+        <>
+            <header
+                className="header"
+                onMouseLeave={() => setActiveSitemap(false)}
+            >
+                <h1>
+                    <a href="/">
+                        <img id="header__logo-img" src={blackLogo} alt="펙센" />
+                    </a>
+                </h1>
+                <nav>
+                    <h2 className="sr-only">메인 메뉴</h2>
+                    <ul id="gnb">
+                        <li
+                            className="gnb__menu"
+                            onMouseEnter={() => {
+                                setActiveMenu("company-introduction");
+                                setActiveSitemap(true);
+                            }}
+                        >
+                            <a className="gnb__link" href="">
+                                {t(`gnb.company-introduction`)}
+                            </a>
+                        </li>
+                        <li
+                            className="gnb__menu"
+                            onMouseEnter={() => {
+                                setActiveMenu("network-security");
+                                setActiveSitemap(true);
+                            }}
+                        >
+                            <a className="gnb__link" href="">
+                                {t(`gnb.network-security`)}
+                            </a>
+                        </li>
+                        <li
+                            className="gnb__menu"
+                            onMouseEnter={() => {
+                                setActiveMenu("cloud");
+                                setActiveSitemap(true);
+                            }}
+                        >
+                            <a className="gnb__link" href="">
+                                {t(`gnb.cloud`)}
+                            </a>
+                        </li>
+                        <li
+                            className="gnb__menu"
+                            onMouseEnter={() => {
+                                setActiveMenu("security-solutions");
+                                setActiveSitemap(true);
+                            }}
+                        >
+                            <a className="gnb__link" href="">
+                                {t(`gnb.security-solutions`)}
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <label id="language-change" htmlFor="language-btn">
+                    <MdLanguage id="language-img" />
+                    <input
+                        type="button"
+                        id="language-btn"
+                        value={currentLanguage}
+                        onClick={changeLanguage}
+                    />
+                </label>
+            </header>
+            <Sitemap
+                activeMenu={activeMenu}
+                activeSitemap={activeSitemap}
+                setActiveSitemap={setActiveSitemap}
+            />
+        </>
     );
 }
 
