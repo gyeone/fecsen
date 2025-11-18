b75986d3debe39dc3ead4e569fa58af979811e6d
gyeone <voov000@naver.com>
2025-11-18 15:47:06 +0900

    feat/index.html의 title태그와 meta태그의 description을 현재 언어에 따라 변하도록 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Main from "./pages/Main.jsx";
 import Footer from "./components/Footer.jsx";
+import { useTranslation } from "react-i18next";
 
 function App() {
+    const { t } = useTranslation();
+
     return (
         <BrowserRouter basename="/fecsen">
-            <Header />
-            <Routes>
-                <Route path="/" element={<Main />} />
-            </Routes>
-            <Footer />
-        </BrowserRouter>
+        <>
+            <title>{t(`head.title`)}</title>
+            <meta name="description" content={t(`head.description`)} />
+
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </>
     );
 }
 
